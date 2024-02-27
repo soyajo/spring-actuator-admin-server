5fdbedaabd26a540848a5d2cb9dd1aac5609de39
JOSOYA <whthdi4693@naver.com>
2024-02-27 10:37:40 +0900

    spring actuator admin server commit

---
+++ src/test/java/com/soya/actuatoradminserver/ActuatorAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soya.actuatoradminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
